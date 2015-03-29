4cfdf8bcab216fe1cc84c8f0680621d21ed9dc73
John Dittmar <John.dittmar@gmail.com>
2015-03-29 04:36:38 -0500

    Added disableplugins feature

--- easydebug.php
+++ easydebug.php
@@ -1,27 +1,41 @@
 <?php
 /**
  * Plugin Name: Easy Debug
- * Description: Easily disable plugins and switch to a default theme using query strings. 
+ * Description: Easily disable plugins and switch to a default theme using query strings.
  * Version: 0.0.1
  * Author: Jason Stallings
  * Author URI: http://github.com/octalmage/
  */
 
-add_action("plugins_loaded","easydebug_filters");
+add_action("init","easydebug_filters");
 
-function easydebug_filters ()
+add_filter("pre_option_active_plugins", "easydebug_plugins");
+add_filter("pre_site_option_active_sitewide_plugins", "easydebug_plugins");
+
+function easydebug_filters()
 {
-    add_filter("template", "easydebug");
-    add_filter("stylesheet", "easydebug");
+    add_filter("template", "easydebug_theme");
+    add_filter("stylesheet", "easydebug_theme");
 }
 
-function easydebug($theme)
+function easydebug_theme($theme)
 {
     if ($_GET["defaulttheme"]==1)
-    {
-            return "twentyfifteen";
-    }
+    {   
+        return "twentyfifteen";
+    }   
     return $theme;
+}
+
+
+function easydebug_plugins($plugins)
+{
+    if ($_GET["disableplugins"]==1)
+    {   
+        return array();
+    }   
+
+    return $plugins;
 }
 
 ?>
